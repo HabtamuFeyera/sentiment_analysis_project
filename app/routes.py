816d12ce14867da118c2bee272d7e90e796bd21d
HabtamuFeyera <habtamufeyer02@gmail.com>
2024-05-15 11:16:27 +0300

    Description of changes

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,20 @@
-from flask import Blueprint, request, jsonify, render_template
-from transformers import BertTokenizer
-from .model.py import SentimentModel
+from flask import Blueprint, request, jsonify
+from transformers import pipeline
 
 main = Blueprint('main', __name__)
 
-# Load model and tokenizer
-model_path = "path_to_saved_model"
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = SentimentModel(model_path, tokenizer)
-
-@main.route('/')
-def index():
-    return render_template('index.html')
+# Load pre-trained sentiment analysis pipeline
+sentiment_pipeline = pipeline("sentiment-analysis")
 
 @main.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     text = data['text']
-    sentiment = model.predict(text)
-    return jsonify({'sentiment': sentiment})
+    
+    # Analyze sentiment
+    result = sentiment_pipeline(text)
+    label = result[0]['label']
+    
+    return jsonify({'sentiment': label})
+
+# Other routes and functions...
